2c871c6d978b7decad4d915c8ada870061e48dd1
elmondino <elmonds@hotmail.co.uk>
2021-04-28 18:24:22 +0100

    updated my-notes page

--- pages/notes/my-notes.js
+++ pages/notes/my-notes.js
@@ -2,18 +2,9 @@ import PersonalNotes from "../../components/Notes/PersonalNotes";
 import CreateNotes from "../../components/Notes/CreateNotes";
 import { useSession } from "next-auth/client";
 import { Heading } from "@chakra-ui/layout";
-import { useState, useEffect } from "react";
 
 export default function MyNotesPage() {
   const [session, loading] = useSession();
-  const [notes, setNotes] = useState();
-
-  useEffect(async () => {
-    const response = await fetch(`/api/note/find-user-notes`);
-    const data = await response.json();
-    console.log(data);
-    setNotes(data.notes);
-  }, []);
 
   if (!session) {
     return (
@@ -26,12 +17,8 @@ export default function MyNotesPage() {
   if (session) {
     return (
       <>
-        {notes && (
-          <>
-            <PersonalNotes notes={notes} setNotes={setNotes} />
-            <CreateNotes notes={notes} setNotes={setNotes} />
-          </>
-        )}
+        <PersonalNotes />
+        <CreateNotes />
       </>
     );
   }
